e7c7d0ed32fcdae0eaca84d679068776d4f5b61c
DEPRI <nrostotopik@gmail.comlkjhklkoinm]sdd>
2021-08-13 19:40:03 +0300

    hw

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,17 @@
-fizzbuzz.py
+import sys 
+numbers = []
+with open('numbers.txt', 'r') as f:
+    items = f.read().split('\n')
+    for i in items:
+        numbers.append([int(n) for n in i.split(',')])
+my_file = open('result.txt', 'w')
+for nums in numbers:
+    for num in range(1, nums[2]+1):
+        if num % nums[0] == 0 and num % nums[1] == 0:
+            print('FB', end = ' ')
+        elif num % nums[0] == 0:
+            print('F', end = ' ')
+        elif num % nums[1] == 0:
+            print('B', end = ' ')
+        else:
+            print(num, end = ' ')
